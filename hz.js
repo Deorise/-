c7d1f938071687054ccbdcf9fa5bffaab4cc5172
Deorise <129961772+Deorise@users.noreply.github.com>
2023-11-20 12:05:42 +0300

    Add files via upload

---
+++ hz.js
@@ -0,0 +1,7 @@
+const secretbtn = document.getElementById('secret-btn')
+const wrapper = document.getElementById('wrapper')
+
+secretbtn.onclick = function () {
+    wrapper.style.filter = 'drop-shadow(0px 0px 30px rgb(209, 59, 209))';
+ 
+}
